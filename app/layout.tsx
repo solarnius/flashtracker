d3d77d07a9d333ee7010c4da2e6f8b99cf5b3861
solarnius <solarnius@sagods.com>
2024-03-22 03:27:26 -0700

    add ref link

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,9 +22,23 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="flex w-full justify-center">
           <main className="flex justify-center flex-col gap-4 mt-4 w-full max-w-2xl mx-8">
-            <h1 className="tracking-tighter font-bold text-2xl">
-              ⚡ flash tracker
-            </h1>
+            <div className="flex w-full items-center justify-between">
+              <h1 className="tracking-tighter font-bold text-2xl">
+                ⚡ flash tracker
+              </h1>
+
+              <a
+                href="https://beast.flash.trade?referral=Beast_1373"
+                target="blank"
+                className="text-black p-2 rounded text-sm"
+                style={{
+                  background:
+                    "linear-gradient(94.61deg,#fffaf3 -4.98%,#fff200 32.6%,#01e1e0 114.17%)",
+                }}
+              >
+                Save 5% on Fees
+              </a>
+            </div>
             <Search />
             {children}
           </main>
