9752ef085733b338b7c45b733b1f4dbd06906429
solarnius <solarnius@sagods.com>
2024-03-22 00:40:03 -0700

    add links

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
 export default function Home() {
   return (
     <p className="text-slate-400">
-      this is a beta product designed to help you see your profitability on
-      flash trade over time. it only supports SOL/BTC/ETH at the moment and may
-      be very buggy, please report bugs to @solarnius on twitter.
+      this is a beta product designed to help you see your profitability on{" "}
+      <a
+        href="https://beast.flash.trade?referral=Beast_1373"
+        target="_blank"
+        className="underline"
+      >
+        flash trade
+      </a>{" "}
+      over time. it only supports SOL/BTC/ETH at the moment and may be very
+      buggy, please report bugs to{" "}
+      <a
+        href="https://twitter.com/solarnius"
+        target="_blank"
+        className="underline"
+      >
+        @solarnius
+      </a>{" "}
+      on twitter.
     </p>
   );
 }
