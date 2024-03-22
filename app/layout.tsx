579f18f3411628bf7ca78ec19ee15fb1219acaf8
solarnius <solarnius@sagods.com>
2024-03-22 00:50:35 -0700

    lightning fast

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <div className="flex w-full justify-center">
           <main className="flex justify-center flex-col gap-4 mt-4 w-full max-w-2xl mx-8">
             <h1 className="tracking-tighter font-bold text-2xl">
-              flash tracker
+              ⚡ flash tracker
             </h1>
             <Search />
             {children}
